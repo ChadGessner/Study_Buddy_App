7051cdfc3db595d3da018f00540eb405d3cb746f
Jason-Stewart138 <88158979+Jason-Stewart138@users.noreply.github.com>
2023-02-25 12:15:13 -0500

    Fixed method GetAllFavoriets

--- StudyBuddy/StudyBuddy/DAL/ApplicationDbContext.cs
+++ StudyBuddy/StudyBuddy/DAL/ApplicationDbContext.cs
@@ -103,7 +103,7 @@ namespace StudyBuddy.DAL
 
     public List<Study> GetAllFavorites(int userId)
     {
-      List<Study> favorites = Favorites.Where(x => x.UserId == userId).Select(x => GetStudy(x.StudyId)).ToList();
+      List<Study> favorites = Favorites.ToList().Where(x => x.UserId == userId).Select(x => GetStudy(x.StudyId)).ToList();
       if (favorites == null)
       {
         return null;
