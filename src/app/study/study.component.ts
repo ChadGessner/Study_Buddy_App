fc11ec2daaaeab3a772f6290bc79bd5dfc99fe18
Jason-Stewart138 <88158979+Jason-Stewart138@users.noreply.github.com>
2023-02-27 21:14:23 -0500

    More formatting

--- src/app/study/study.component.ts
+++ src/app/study/study.component.ts
@@ -13,45 +13,41 @@ export class StudyComponent implements OnInit {
   @Input() study: Study | null = null;
   isCanHasAnswer: boolean = false;
   isCanHasPicaard: boolean = true;
-
   @Input() loggedInUser: LoggedInUser | null = null;
   @Input() index: number = 0;
   @Output() clicked: EventEmitter<boolean> = new EventEmitter<boolean>();
+  
   constructor(private api: ApiService, private render: Renderer2, el: ElementRef) { }
+
   notIsCanAnswer(e: MouseEvent) {
     let target = e.target as HTMLElement
     if (target && target.classList.value === 'bi bi-star-fill') {
       let parent = target.parentElement?.parentElement as HTMLHeadingElement;
       let question = parent.innerText.trim().toLowerCase();
 
-
       if (this.loggedInUser && this.study) {
         let id = this.study.id
         this.api.selectFavorite(id);
-
       }
-
-
       return;
-
     }
     if (this.study && !this.isCanHasAnswer && this.study.answer === 'THERE ARE FOUR LIGHTS!') {
-      //console.log(this.isCanHasPicaard);
-
+            //console.log(this.isCanHasPicaard);
       this.fourLights(this.study.answer)
     }
     this.isCanHasAnswer = !this.isCanHasAnswer;
   }
+
   fravritClicked() {
     if (this.loggedInUser) {
       return this.clicked.emit(true);
     }
     return;
   }
+
   fourLights(answer: string | null | undefined) {
     let url = "https://i.imgur.com/mKtwyFr.jpg";
     let node = document.getElementsByTagName('h1')[0]
-
     if (node) {
       node.innerText = ''
       let img = this.render.createElement('img');
@@ -66,10 +62,9 @@ export class StudyComponent implements OnInit {
     }
     return;
   }
-  ngOnInit(): void {
 
+  ngOnInit(): void {
     this.api.loggedInEvent.subscribe((x) => this.loggedInUser = x);
     this.api.onComponentLoad();
-
   }
 }
